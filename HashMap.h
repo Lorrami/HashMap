180edf629286454877d8bdf2d93796a7f594f0a3
Lorrami <wwwromchelo040608@gmail.com>
2022-03-23 18:34:39 +0100

    A lot of updates

--- HashMap.h
+++ HashMap.h
@@ -1,4 +1,75 @@
-class HashMap
-{
+#pragma once
 
+#include "Vector/Vector.h"
+
+template <typename Key, typename Value>
+struct HashMap {
+private:
+    struct Node {
+        Key key;
+        Value value;
+    };
+
+    int m_Capacity = 5;
+    Node **m_Table = nullptr;
+public:
+    HashMap();
+    //~HashMap();
+    void Add(Key key, Value value);
+    void Remove(Key key);
+    Value &Find(Key key);
+    void Print();
 };
+
+template<typename Key, typename Value>
+HashMap<Key, Value>::HashMap() {
+    m_Table = new Node*[m_Capacity];
+    for(int i = 0; i < m_Capacity; i++)
+        m_Table[i] = nullptr;
+}
+
+/*template<typename Key, typename Value>
+HashMap<Key, Value>::~HashMap() {
+    delete[] Map;
+
+}*/
+
+template<typename Key, typename Value>
+void HashMap<Key, Value>::Add(Key key, Value value) {
+    std::hash<Key> hasher;
+    size_t hash = hasher(key) % m_Capacity;
+
+    Node *newNode = new Node();
+    newNode->key = key;
+    newNode->value = value;
+
+    m_Table[hash] = newNode;
+}
+
+template<typename Key, typename Value>
+void HashMap<Key, Value>::Remove(Key key) {
+    std::hash<Key> hasher;
+    size_t hash = hasher(key) % m_Capacity;
+
+    delete m_Table[hash];
+    m_Table[hash] = nullptr;
+}
+
+template<typename Key, typename Value>
+Value &HashMap<Key, Value>::Find(Key key) {
+    std::hash<Key> hasher;
+    size_t hash = hasher(key) % m_Capacity;
+
+    return m_Table[hash]->value;
+}
+
+template<typename Key, typename Value>
+void HashMap<Key, Value>::Print() {
+    for (int i = 0; i < m_Capacity; i++)
+    {
+        if(m_Table[i])
+            std::cout << m_Table[i]->key << " --> " << m_Table[i]->value << std::endl;
+        else
+            std::cout << "nullptr" << std::endl;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include "HashMap.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main()
+{
+    HashMap<int, std::string> Map;
+
+    Map.Add(4, "Lorrami");
+    Map.Add(1, "BloodRedTape");
+    Map.Add(16, "Jeytery");
+    Map.Add(69, "Abobus");
+
+    Map.Remove(1);
+
+    std::cout << Map.Find(16) << std::endl;
+
+    Map.Print();
+
+    return 777;
 }
