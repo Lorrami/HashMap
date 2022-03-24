0e485ac5dbdd101d61ee5c54137381fe6ca21d99
Lorrami <wwwromchelo040608@gmail.com>
2022-03-24 11:18:57 +0100

    Add resizing

--- HashMap.h
+++ HashMap.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "Vector/Vector.h"
 #include "LinkedList/LinkedList.h"
 
 template <typename Key, typename Value>
@@ -11,8 +10,11 @@ private:
         Value value;
     };
 
-    int m_Capacity = 5;
+    int m_Capacity = 3;
+    int m_Size = m_Capacity;
     LinkedList<Node> *m_Table = nullptr;
+
+    void AddMemory();
 public:
     HashMap();
     //~HashMap();
@@ -26,24 +28,40 @@ template<typename Key, typename Value>
 HashMap<Key, Value>::HashMap() {
     m_Table = new LinkedList<Node>[m_Capacity];
 }
-
 /*template<typename Key, typename Value>
 HashMap<Key, Value>::~HashMap() {
     delete[] Map;
 
 }*/
-
+template <typename Key, typename Value>
+void HashMap<Key, Value>::AddMemory() {
+    m_Capacity *= 2;
+    LinkedList<Node> *tmp =m_Table;
+    m_Table = new LinkedList<Node>[m_Capacity];
+    for (auto i = 0; i < m_Size; ++i) {
+        for (auto it : tmp[i]) {
+            std::hash<Key> hasher;
+            size_t hash = hasher(it.key) % m_Capacity;
+            m_Table[hash].Add(it);
+        }
+    }
+    delete[] tmp;
+}
 template<typename Key, typename Value>
 void HashMap<Key, Value>::Add(Key key, Value value) {
     std::hash<Key> hasher;
     size_t hash = hasher(key) % m_Capacity;
 
+    if (m_Size >= m_Capacity)
+        AddMemory();
+
     Node *newNode = new Node;
     newNode->key = key;
     newNode->value = value;
     m_Table[hash].Add(*newNode);
-}
 
+    m_Size++;
+}
 template<typename Key, typename Value>
 void HashMap<Key, Value>::Remove(Key key) {
     std::hash<Key> hasher;
@@ -51,8 +69,9 @@ void HashMap<Key, Value>::Remove(Key key) {
 
     delete m_Table[hash];
     m_Table[hash] = nullptr;
-}
 
+    m_Size--;
+}
 template<typename Key, typename Value>
 Value &HashMap<Key, Value>::Find(Key key) {
     std::hash<Key> hasher;
@@ -60,14 +79,14 @@ Value &HashMap<Key, Value>::Find(Key key) {
 
     return m_Table[hash]->value;
 }
-
-template<typename Key, typename Value>
+template <typename Key, typename Value>
 void HashMap<Key, Value>::Print() {
     for (int i = 0; i < m_Capacity; i++) {
         if (m_Table[i].Size() > 0) {
             for (auto it = m_Table[i].begin(); it != m_Table[i].end(); ++it) {
                 std::cout << it->key << " --> " << it->value << "  ";
             }
+            std::cout << std::endl;
         }
         else
             std::cout << "nullptr" << std::endl;
--- main.cpp
+++ main.cpp
@@ -6,7 +6,18 @@ int main()
     HashMap<int, std::string> Map;
 
     Map.Add(4, "Lorrami");
-    Map.Add(11, "BloodRedTape");
+    Map.Add(14, "BloodRedTape");
+    Map.Add(17, "Jeytery");
+    Map.Add(35, "Abobus");
+    Map.Add(69, "KEp");
+    Map.Add(71, "tteaman");
+    Map.Add(83, "Andrew");
+    Map.Add(89, "Dasha");
+    Map.Add(101, "Gavnuke");
+    Map.Add(134, "Klukwish");
+    Map.Add(146, "Chel1");
+    Map.Add(800, "Content");
+
 
     Map.Print();
 
